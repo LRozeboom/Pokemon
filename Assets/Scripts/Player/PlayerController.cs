d38dc6d1bbbd9aa0e09b2a5ea1197cd2e7d61e53
Luuk Rozeboom <luukrozeboom@outlook.com>
2023-07-02 13:30:31 +0200

    Finished #1 setup and tile based movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -14,23 +14,33 @@ public class PlayerController : MonoBehaviour
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
-        }
 
-        if (input != Vector2.zero)
-        {
-            var targetPos = transform.position;
-            targetPos.x += input.x;
-            targetPos.y += input.y;
+
+            // Remove diagonal movement
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
         }
     }
 
     IEnumerator Move(Vector3 targetPos)
     {
+        isMoving = true;
+
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
             yield return null;
         }
         transform.position = targetPos;
+
+        isMoving = false;
     }
 }
