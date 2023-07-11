2c25c2506190ff30ee36e11702af7db711470783
Luuk Rozeboom <luukrozeboom@outlook.com>
2023-07-11 20:08:14 +0200

    Implemented attacks in battle system.

--- Assets/Scripts/Pokemons/Pokemon.cs
+++ Assets/Scripts/Pokemons/Pokemon.cs
@@ -59,4 +59,31 @@ public class Pokemon
         get { return Mathf.FloorToInt((Base.MaxHp * Level) / 100f) + 10; }
     }
 
+    /// <summary>
+    /// Let's the pokemon take damage from an enemy pokemon move.
+    /// </summary>
+    /// <param name="move">The move that the pokemon takes damage from.</param>
+    /// <param name="attacker">The attacker that performs the move.</param>
+    /// <returns>Boolean value whether the pokemon has fainted.</returns>
+    public bool TakeDamage(Move move, Pokemon attacker)
+    {
+        float modifiers = Random.Range(0.85f, 1f);
+        float a = (2 * attacker.Level + 10) / 250f;
+        float d = a * move.Base.Power * ((float)attacker.Attack / Defense) + 2;
+        int damage = Mathf.FloorToInt(d * modifiers);
+
+        HP -= damage;
+        if (HP <= 0)
+        {
+            HP = 0;
+            return true;
+        }
+        return false;
+    }
+
+    public Move GetRandomMove()
+    {
+        int r = Random.Range(0, Moves.Count);
+        return Moves[r];
+    }
 }
