c1b27b2d99d4820d512533b71f57fc70b83b4ad1
Luuk Rozeboom <luukrozeboom@outlook.com>
2023-07-20 21:44:57 +0200

    Added sleep, paralyze and freeze status conditions

--- Assets/Scripts/Data/ConditionsDB.cs
+++ Assets/Scripts/Data/ConditionsDB.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public class ConditionsDB
 {
@@ -27,6 +28,68 @@ public class ConditionsDB
                 {
                     pokemon.UpdateHP(pokemon.MaxHp / 16);
                     pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name} hurt itself due to burn");
+                }
+            }
+        },
+        {
+            ConditionID.par,
+            new Condition()
+            {
+                Name = "Paralyzed",
+                StartMessage = "has been paralyzed",
+                OnBeforeMove = (Pokemon pokemon) =>
+                {
+                    if (Random.Range(1, 5) == 1)
+                    {
+                        pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name}'s paralyzed and can't move");
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
+        },
+        {
+            ConditionID.frz,
+            new Condition()
+            {
+                Name = "Paralyzed",
+                StartMessage = "has been frozen",
+                OnBeforeMove = (Pokemon pokemon) =>
+                {
+                    if (Random.Range(1, 5) == 1)
+                    {
+                        pokemon.CureStatus();
+                        pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name} is not frozen any more");
+                        return true;
+                    }
+
+                    return false;
+                }
+            }
+        },
+        {
+            ConditionID.slp,
+            new Condition()
+            {
+                Name = "Sleep",
+                StartMessage = "has fallen asleep",
+                OnStart = (Pokemon pokemon) =>
+                {
+                    pokemon.StatusTime = Random.Range(1, 4);
+                    Debug. Log($"Will be asleep for {pokemon.StatusTime} moves");
+                },
+                OnBeforeMove = (Pokemon pokemon) =>
+                {
+                    if (pokemon.StatusTime <= 0)
+                    {
+                        pokemon.CureStatus();
+                        pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name} woke up!");
+                        return true;
+                    }
+                    pokemon.StatusTime--;
+                    pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name} is sleeping");
+                    return false;
                 }
             }
         }
--- Assets/Scripts/Pokemons/Pokemon.cs
+++ Assets/Scripts/Pokemons/Pokemon.cs
@@ -29,6 +29,7 @@ public class Pokemon
     public Dictionary<Stat, int> StatBoosts { get; private set; }
     public Condition Status { get; private set; }
     public Queue<string> StatusChanges { get; private set; } = new();
+    public int StatusTime { get; set; }
     public bool HpChanged { get; set; }
 
     public void Init()
@@ -191,14 +192,30 @@ public class Pokemon
     public void SetStatus(ConditionID conditionId)
     {
         Status = ConditionsDB.Conditions[conditionId];
+        Status?.OnStart?.Invoke(this);
         StatusChanges.Enqueue($"{Base.name} {Status.StartMessage}");
         HpChanged = true;
+    }
+
+    public void CureStatus()
+    {
+        Status = null;
     }
 
     public Move GetRandomMove()
     {
         int r = Random.Range(0, Moves.Count);
         return Moves[r];
+    }
+
+    public bool OnBeforeMove()
+    {
+        if (Status?.OnBeforeMove != null)
+        {
+            return Status.OnBeforeMove(this);
+        }
+
+        return true;
     }
 
     public void OnAfterTurn()
