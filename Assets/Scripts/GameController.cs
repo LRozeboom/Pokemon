5b225266be9b27e5890f21aa6ef5f30fe6ef50e4
Luuk Rozeboom <luukrozeboom@outlook.com>
2023-07-30 19:13:34 +0200

    Added npc dialog

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public enum GameState
 {
     FreeRoam,
-    Battle
+    Battle,
+    Dialog
 }
 
 public class GameController : MonoBehaviour
@@ -23,6 +24,19 @@ public class GameController : MonoBehaviour
     {
         playerController.OnEncountered += StartBattle;
         battleSystem.OnBattleOver += EndBattle;
+
+        DialogManager.Instance.OnShowDialog += () =>
+        {
+            state = GameState.Dialog;
+        };
+
+        DialogManager.Instance.OnCloseDialog += () =>
+        {
+            if (state == GameState.Dialog)
+            {
+                state = GameState.FreeRoam;
+            }
+        };
     }
 
     void StartBattle()
@@ -53,6 +67,10 @@ public class GameController : MonoBehaviour
         else if (state == GameState.Battle)
         {
             battleSystem.HandleUpdate();
+        }
+        else if (state == GameState.Dialog)
+        {
+            DialogManager.Instance.HandleUpdate();
         }
     }
 }
